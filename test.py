9f9b1cf1f43290f782a0ce876163df420cd86045
edouard-torres <edouard.torres05@gmail.com>
2024-12-05 19:32:34 +0100

    toucher couler

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-from random import *
-from time import sleep
-def grille_vide():
-    M = [[],[],[],
-         [],[],[],
-         [],[],[]]
-    return M
+def tour(joueur, grille_tirs_joueur, grille_adversaire) :
+    if joueur == 0:
+        print("C'est à votre tour de faire feu !:")
+    else:
+        print("C'est le tour du maître du jeu :")
 
-def affiche_grille(grille,message):
-    if message=="Rappel de l'historique des tirs que vous avez effectués :":
-        print(message)
-        gri=grille
-        for i in range(len(gri)):
-            for j in range(len(gri[i])):
-                gri[i][j].append(" | ")
-            gri[i].append(" | ")
-        print(gri)
+    affiche_grille(grille_tirs_joueur, "Rappel de l'historique des tirs que vous avez effectués :")
 
+    if joueur == 0:
+        ligne, colonne = demande_position()
+    else:
+        ligne, colonne = random.randint(0, 2), random.randint(0, 2)
+        print(f"Le maître du jeu tire en position {ligne + 1},{colonne + 1}")
 
-print(affiche_grille(grille_vide(),"Rappel de l'historique des tirs que vous avez effectués :"))
+    if grille_adversaire[ligne][colonne] == 'B':
+        print("Touché coulé !")
+        grille_tirs_joueur[ligne][colonne] = 'x'
+        grille_adversaire[ligne][colonne] = ' '
+    else:
+        print("Dans l'eau...")
+        grille_tirs_joueur[ligne][colonne] = '.'
